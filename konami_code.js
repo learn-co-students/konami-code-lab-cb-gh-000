05efac7c8e48187b8e93403a0e70daaec6caa7ff
Rafid Aslam <rafidteam@gmail.com>
2017-02-23 21:32:07 +0700

    Done

--- konami_code.js
+++ konami_code.js
@@ -1,5 +1,20 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
+var index = 0;
+
+function konamicode(e) {
+    if (e.which === code[index]) {
+        index++;
+
+        if (index === code.length){
+            alert('You did it!!');
+            index = 0;
+        }
+    } else {
+        index = 0;
+    }
+}
+
 function init() {
-  // your code here
+  document.body.addEventListener('keydown', konamicode)
 }
