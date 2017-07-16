3998e2a2363111397f62954691fe21d21cd51379
Jamie Roberts <ll13j7r@leeds.ac.uk>
2017-07-16 18:33:10 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -2,5 +2,21 @@ const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
 
 function init() {
   // Write your JavaScript code inside the init() function
+  let index = 0;
+  const input = document.querySelector('body')
+  input.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    console.log(key);
+    if (key == code[index]) {
+      index ++;
+      e.preventDefault()
 
+      if (index === code.length - 1) {
+        alert("boo");
+        index = 0;
+      }
+    }else{
+      index = 0;
+    }
+  })
 }
