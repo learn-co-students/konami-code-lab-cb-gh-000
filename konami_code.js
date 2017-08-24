2f49f98f9ef95d22d2977c97ea6f9b94597d05c2
Jhondee Diaz <jhondeediaz@gmail.com>
2017-08-24 07:12:49 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,14 @@
 const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
-
+var index = 0;
 function init() {
   // Write your JavaScript code inside the init() function
-
+  document.addEventListener('keydown', function(e){
+    if(parseInt(e.which,10) == code[index]|| parseInt(e.detail,10) == code[index]){
+      index++
+      if(code.length == index)
+        alert('CONGRATS!')
+    }else{
+      index = 0;
+    }
+  });
 }
