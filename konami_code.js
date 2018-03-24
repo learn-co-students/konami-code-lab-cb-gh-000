49668b225c0da499c3c8fb45db6d2c3325d2127e
Gary Thirkell <garythirkell@gmail.com>
2018-03-24 14:05:46 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,6 +1,29 @@
-const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65]
+// Up, Up, Down, Down, Left, Right, Left, Right, B, A
+const code = [38, 38, 40, 40, 37, 39, 37, 39, 66, 65];
+let keySequence = [];
 
 function init() {
-  // Write your JavaScript code inside the init() function
-
+  document.body.addEventListener('keydown', function(e) {
+    const key = parseInt(e.detail || e.which);
+    updateKeySequence(key, checkCode);
+  });
 }
+
+function updateKeySequence(key, checkCode) {
+  keySequence.push(key);
+  checkCode();
+}
+
+function checkCode() {
+  for (let i = 0; i < keySequence.length; i++) {
+    if (keySequence[i] === code[i]) {
+      if (keySequence.length === code.length) {
+        alert('Konami!');
+      }
+    } else {
+      keySequence = [];
+    }
+  }
+}
+
+init();
