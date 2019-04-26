94219375e402e016b05dd961e41139176f507360
IVAN V <ivanvalette@hotmail.com>
2019-04-26 01:16:22 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -1,16 +1,34 @@
 const codes = [
-  "ArrowUp",
-  "ArrowUp",
-  "ArrowDown",
-  "ArrowDown",
-  "ArrowLeft",
-  "ArrowRight",
-  "ArrowLeft",
-  "ArrowRight",
-  "b",
-  "a"
+  38,
+  38,
+  40,
+  40,
+  37,
+  39,
+  37,
+  39,
+  66,
+  65
 ];
 
+var counter = 0;
 function init() {
-  // your code here
+  var body = document.getElementsByTagName('body')[0];
+  body.addEventListener('keydown', (e)=>{
+
+      if(e.which === codes[counter])
+      {
+        counter++;
+        if(counter == codes.length)
+        {
+          alert("Hurray!")
+          counter = 0;
+        }
+      }
+      else {
+        counter = 0;
+      }
+
+
+  })
 }
