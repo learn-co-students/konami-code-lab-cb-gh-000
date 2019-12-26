c0c7b169471a3ea3e09bc6fd51a1d0fb834b592a
Ri Xin Yang <rixin.yang.2001@gmail.com>
2019-12-26 21:42:00 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -12,5 +12,22 @@ const codes = [
 ];
 
 function init() {
-  // your code here
+  let index = 0;
+
+  document.body.addEventListener('keydown', function(e) {
+    const key = e.key;
+    console.log(e.key);
+    console.log(index);
+
+    if (key === codes[index]) {
+      ++index;
+      if (index == codes.length) {
+          alert('Such good!');
+          index = 0;
+      }
+    }
+    else {
+      index = 0;
+    }
+  });
 }
