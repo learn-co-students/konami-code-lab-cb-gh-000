058da328a4909b4f06b8b4167c624f1f246cdf7d
Armintas Zadeika <az778@york.ac.uk>
2020-05-18 14:05:45 +0000

    Done.

--- konami_code.js
+++ konami_code.js
@@ -10,7 +10,22 @@ const codes = [
   "b",
   "a"
 ];
-
+let index=0;
 function init() {
-  // your code here
-}
+  document.body.addEventListener("keydown",function(e){
+    const key = e.key;
+    if(key==codes[index]){
+      index+=1;
+      console.log(key);
+      console.log(index);
+
+      if(index==codes.length){
+        alert("Hurray");
+        index=0;
+      }
+    }
+    else{
+      index=0;
+    }
+  })
+};
